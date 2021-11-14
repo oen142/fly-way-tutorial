3c5cfe89c4c8259e323d9afde531086a29f6986b
oen142 <oen142@naver.com>
2021-11-14 20:35:52 +0900

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,59 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.flywaydb.flyway") version "7.8.1"
+    id("org.springframework.boot") version "2.5.6"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    kotlin("jvm") version "1.5.31"
+    kotlin("plugin.spring") version "1.5.31"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    runtimeOnly("mysql:mysql-connector-java")
+    implementation("org.flywaydb:flyway-core:7.8.1")
+
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "11"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+
+/*
+flyway {
+    url = System.getenv("DB_URL")
+    user = System.getenv("DB_USER")
+    password = System.getenv("DB_PASSWORD")
+    baselineOnMigrate = true
+    locations = arrayOf("filesystem: resources / db / migration")
+}*/
+
+flyway {
+    url = "jdbc:mysql://localhost:3306/db_example_test"
+    user = "test"
+    password = "Password#1"
+    locations = arrayOf("filesystem:src/test/resources/db/migration")
+}
---
+++ src/main/kotlin/com/example/flyway/FlyWayApplication.kt
@@ -0,0 +1,11 @@
+package com.example.flyway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FlyWayApplication
+
+fun main(args: Array<String>) {
+    runApplication<FlyWayApplication>(*args)
+}
